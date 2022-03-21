7b3d670ffdfd1caf5bda2f6044301f8d83a1389f
kentuckleberry <101270426+kentuckleberry@users.noreply.github.com>
2022-03-20 20:45:09 -0400

    Update FerQuerenSea.sol
    fergots all the stufff that goes at the top

--- FerQuerenSea.sol
+++ FerQuerenSea.sol
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
 /// @custom:security-contact squeakiedork@gmail.com
 contract FerQuerenSea is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
